7adfad37e7fe335b5fa6157346c3558ff76a2262
Gus Adi <81859486+LordZein69@users.noreply.github.com>
2025-10-22 11:28:46 +0800

    Refactor dark overlay creation with MutationObserver

--- js/darken-theme.js
+++ js/darken-theme.js
@@ -1,11 +1,24 @@
-const container = document.querySelector('.videoPlayerContainer');
-const overlay = document.createElement('div');
-overlay.style.cssText = `
-  position: absolute;
-  top: 0; left: 0; width: 100%; height: 100%;
-  background: black;
-  opacity: 0.4;
-  pointer-events: none;
-  z-index: 10;
-`;
-container.appendChild(overlay);
+// you need this to run js in jellyfin : https://github.com/n00bcodr/Jellyfin-JavaScript-Injector
+
+
+(function () {
+  const observer = new MutationObserver(() => {
+    const container = document.querySelector('.videoPlayerContainer');
+    if (container && !container.querySelector('.darkOverlay')) {
+      const overlay = document.createElement('div');
+      overlay.className = 'darkOverlay';
+      overlay.style.cssText = `
+        position: absolute;
+        top: 0; left: 0; width: 100%; height: 100%;
+        background: black;
+        opacity: 0.4;
+        pointer-events: none;
+        z-index: 10;
+      `;
+      container.style.position = 'relative';
+      container.appendChild(overlay);
+    }
+  });
+
+  observer.observe(document.body, { childList: true, subtree: true });
+})();
